c98605d91feeaa405af0b2e683d874e3b1c20b2e
Eric Binnion <ericbinnion@gmail.com>
2018-02-10 12:04:38 -0600

    Setting a foundation; Building header w/out plugin; Remove all home page but slider

--- functions.php
+++ functions.php
@@ -9,23 +9,17 @@ function kareless_full_width( $classes ) {
 	return $classes;
 }
 
-add_action( 'init', 'kareless_init' );
+add_action( 'init', 'kareless_init', 11 );
 function kareless_init() {
-	remove_action( 'homepage', 'storefront_homepage_content', 10 );
-	remove_action( 'homepage', 'storefront_product_categories', 20 );
-	remove_action( 'homepage', 'storefront_featured_products', 40 );
-	remove_action( 'homepage', 'storefront_popular_products', 50 );
-	remove_action( 'homepage', 'storefront_on_sale_products', 60 );
 	remove_action( 'storefront_single_post', 'storefront_post_meta', 20 );
 	remove_action( 'storefront_loop_post', 'storefront_post_meta', 20 );
 	remove_action( 'storefront_footer', 'storefront_credit', 20 );
-	remove_action( 'storefront_header', 'storefront_header_cart' );
+	remove_action( 'storefront_before_content', 'storefront_header_widget_region', 10 );
+	remove_action( 'storefront_header', 'storefront_secondary_navigation',         30 );
 
-	// add_action( 'storefront_header', 'kareless_header_cart' );
+	remove_all_actions( 'homepage' );
 
-	// add_action( 'homepage', 'storefront_homepage_content', 60 );
-
-	add_action( 'storefront_before_content', 'kareless_instagram_header', 10 );
+	add_action( 'homepage', 'kareless_homepage' );
 	add_action( 'storefront_footer', 'kareless_credit', 20 );
 
 	add_filter( 'storefront_recent_products_args', 'kareless_eight_recent' );
@@ -33,12 +27,11 @@ function kareless_init() {
 	add_action( 'wp_enqueue_scripts', 'kareless_remove_sticky_footer', 99 );
 }
 
-
 function kareless_child_scripts() {
-	// wp_enqueue_style( 'kareless-child', get_stylesheet_directory_uri() . '/style.css', array( 'storefront-style' ) );
+	wp_enqueue_style( 'kareless-child', get_stylesheet_directory_uri() . '/style.css', array( 'storefront-style' ) );
 	wp_dequeue_style( 'storefront-child-style-css' );
 }
-add_action( 'wp_enqueue_scripts', 'kareless_child_scripts', 101 );
+// add_action( 'wp_enqueue_scripts', 'kareless_child_scripts', 101 );
 
 
 function kareless_credit() {
@@ -56,31 +49,75 @@ function remove_storefront_sidebar() {
 	}
 }
 
-function kareless_instagram_header() {
-	if ( ! is_front_page() ) return;
-?>
+function kareless_homepage() { ?>
+	<?php if ( function_exists( 'putRevSlider' ) ) : ?>
 	<div id="kareless-instagram-footer">
-		<?php putRevSlider( 'instagramfooter' ); ?>
+		<?php putRevSlider( 'slider1' ); ?>
 	</div>
-<?php
-}
+	<?php endif; ?>
+
+<?php }
 
 function kareless_eight_recent( $args ) {
 	$args['limit'] = 8;
 	return $args;
 }
 
-
-function kareless_about_page_promo() {
-?>
-	<div id="kareless-about-promo">
-
-	</div>
-<?php
-}
-
 function kareless_remove_sticky_footer() {
 	wp_dequeue_script( 'storefront-sticky-payment' );
+}
+
+// ------------------
+// 1. Register new endpoint to use for My Account page
+// Note: Resave Permalinks or it will give 404 error
+
+function kareless_add_aff_wp_endpoint() {
+	add_rewrite_endpoint( 'aff', EP_ROOT | EP_PAGES );
+}
+add_action( 'init', 'kareless_add_aff_wp_endpoint' );
+
+// ------------------
+// 2. Add new query var
+function kareless_aff_wp_query_vars( $vars ) {
+	$vars[] = 'aff';
+	return $vars;
+}
+// add_filter( 'query_vars', 'kareless_aff_wp_query_vars', 0 );
+
+// ------------------
+// 3. Insert the new endpoint into the My Account menu
+function kareless_add_aff_wp_link_my_account( $items ) {
+	if ( function_exists( 'affwp_is_affiliate' ) && affwp_is_affiliate() ) {
+		$logout = array_pop( $items );
+		$items['aff'] = 'Affiliate Area';
+		$items[] = $logout;
+	}
+
+	unset( $items['downloads'] );
+	return $items;
+}
+add_filter( 'woocommerce_account_menu_items', 'kareless_add_aff_wp_link_my_account' );
+
+// ------------------
+// 4. Add content to the new endpoint
+
+function kareless_aff_wp_content() {
+	if ( ! class_exists( 'Affiliate_WP_Shortcodes' ) ) {
+		return;
+	}
+
+	$shortcode = new Affiliate_WP_Shortcodes;
+	echo $shortcode->affiliate_area();
+}
+add_action( 'woocommerce_account_aff_endpoint', 'kareless_aff_wp_content' );
+
+function kareless_filter_aff_tabs( $url, $page_id, $tab ) {
+	return esc_url_raw( add_query_arg( 'tab', $tab ) );
+}
+add_filter( 'affwp_affiliate_area_page_url', 'kareless_filter_aff_tabs', 10, 3 );
+
+function storefront_header_cart() {
+	kareless_header_cart();
 }
 
 function kareless_header_cart() {
@@ -99,3 +136,21 @@ function kareless_header_cart() {
 	<?php
 	}
 }
+
+function ds_checkout_analytics( $order_id ) {
+	$order = new WC_Order( $order_id );
+	$currency = $order->get_order_currency();
+	$total = $order->get_total();
+	$date = $order->order_date;
+	?>
+	<!-- Paste Tracking Code Under Here -->
+<script>
+  fbq('track', 'Purchase', {
+    value: 0.2,
+    currency: USD,
+  });
+</script>
+	<!-- End Tracking Code -->
+	<?php
+}
+add_action( 'woocommerce_thankyou', 'ds_checkout_analytics' );
