2f53ffd8824ebaff37b9607f79f3ba3b0e3cc733
Eric Binnion <ericbinnion@gmail.com>
2018-04-14 21:22:02 -0500

    SASS: Home: Base implementation of equal height/width columns

--- includes/partials/home-body.php
+++ includes/partials/home-body.php
@@ -1,1 +1,12 @@
 <!-- In home-body partial -->
+<div class="kareless-eq-wrap">
+	<div class="kareless-eq-row-item">
+		&nbsp;
+	</div>
+	<div class="kareless-eq-row-item">
+		&nbsp;
+	</div>
+	<div class="kareless-eq-row-item">
+		&nbsp;
+	</div>
+</div>
