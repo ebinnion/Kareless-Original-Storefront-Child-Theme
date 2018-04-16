e5bbbad5d68447472de9ac4b15de93980b67414b
Eric Binnion <ericbinnion@gmail.com>
2018-04-15 21:17:09 -0500

    Home: Pass at styles for home sections

--- includes/partials/home-body.php
+++ includes/partials/home-body.php
@@ -1,26 +1,44 @@
 <div class="kareless-eq-wrap">
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '1a-family/family2.JPG' ) ); ?>"/>
+		<div class="kareless-home-section-content">
+			<a href="#">Kareless Family</a>
+		</div>
 	</div>
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '1a-family/family1.JPG' ) ); ?>" />
+		<div class="kareless-home-section-content">
+			<a href="#">Our Mission Statement</a>
+		</div>
 	</div>
 </div>
 
 <div class="kareless-eq-wrap">
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '2-radio-bg/radio1.JPG' ) ); ?>"/>
+		<div class="kareless-home-section-content">
+			<a href="#">KRLS Radio/Podcast</a>
+		</div>
 	</div>
 </div>
 
 <div class="kareless-eq-wrap">
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '3-product/product8.jpeg' ) ); ?>"/>
+		<div class="kareless-home-section-content">
+			<a href="#">Accessories</a>
+		</div>
 	</div>
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '3-product/product3.JPG' ) ); ?>"/>
+		<div class="kareless-home-section-content">
+			<a href="#">Headwear </a>
+		</div>
 	</div>
 	<div class="kareless-eq-row-item">
 		<img class="kareless-image-fill" src="<?php echo esc_url( kareless_image_url( '3-product/product7.JPG' ) ); ?>"/>
+		<div class="kareless-home-section-content">
+			<a href="#">Apparel</a>
+		</div>
 	</div>
 </div>
