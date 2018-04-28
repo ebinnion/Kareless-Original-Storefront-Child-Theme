464d037c240276e42acbc916c29cc0c7662428f9
Eric Binnion <ericbinnion@gmail.com>
2018-04-27 21:21:35 -0500

    Home: JS: Remove unnecessary comment

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,3 @@
-// ( function( $, ofi ) {
-// 	ofi( $( 'img.kareless-image-fill' ) );
-// } )( jQuery, objectFitImages );
-
 jQuery( function () {
 	objectFitImages( jQuery(
 		'.kareless-image-fill-container img'
